a63451ed67f96244649b520c34ae49fb7f24aa19
ihsanlatheef <ihsirn@hotmail.com>
2024-11-07 12:26:32 +0000

    Implemented MSAL authentication

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { loginRequest } from "./auth/AuthConfig";
+
+function App() {
+  const { instance } = useMsal();
+  const activeAccount = instance.getActiveAccount();
+
+  const handleLoginRedirect = () => {
+    instance
+      .loginRedirect({
+        ...loginRequest,
+        prompt: "login",
+      })
+      .catch((error: unknown) => console.log(error));
+  };
+
+  const handleLogoutRedirect = () => {
+    instance.logoutPopup({ postLogoutRedirectUri: "/" });
+    window.location.reload();
+  };
+
+  return (
+    <div className="card">
+      <AuthenticatedTemplate>
+        {activeAccount ? (
+          <>
+            <button onClick={handleLogoutRedirect}>Logout</button>
+            <p>You are signed in!</p>
+          </>
+        ) : null}
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <>
+          <button onClick={handleLoginRedirect}>Login</button>
+          <p>Please sign in!</p>
+        </>
+      </UnauthenticatedTemplate>
+    </div>
+  );
+}
+
+export default App;
